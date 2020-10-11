06f93a13e2088567a89c570a94d71313498439c2
fLa <flatra66@gmail.com>
2020-10-12 04:02:19 +0500

    add slots and eslint remove components, empty row props, totalRowsCountKey prop

--- src/js/components/loading_row.vue
+++ src/js/components/loading_row.vue
@@ -1,7 +1,9 @@
 <template>
   <tr>
-    <td :colspan="columnsLength" :class="['vue-slim-tables-loading-row', `vue-slim-tables-loading-row-${randNum}`]">
-      <div></div>
+    <td
+      :colspan="columnsLength"
+      :class="['vue-slim-tables-loading-row', `vue-slim-tables-loading-row-${randNum}`]">
+      <div />
     </td>
   </tr>
 </template>
@@ -9,12 +11,12 @@
 <script>
 export default {
   props: {
-    columnsLength: { type: Number, required: true }
+    columnsLength: { type: Number, required: true },
   },
   data() {
     return {
-      randNum: Math.floor(Math.random() * 10) + 1
+      randNum: Math.floor(Math.random() * 10) + 1,
     }
-  }
+  },
 }
 </script>
--- src/js/components/table.vue
+++ src/js/components/table.vue
@@ -1,24 +1,59 @@
 <template>
   <div :class="wrapperClass">
     <table :class="tableClass" class="slim-table">
-      <thead>
+      <thead v-if="columns.length">
         <tr>
-          <th v-for="col in columns" class="slim-table-th" @click.prevent="col.orderable ? onOrderClick(col.key) : null">
-            <span>{{ col.title }}</span>
-            <a v-if="col.orderable" :class="`slim-table-orderable ${orders[col.key] || ''}`"></a>
+          <th
+            v-for="column in columns"
+            :key="column.key"
+            class="slim-table-th"
+            @click.prevent="column.orderable ? onOrderClick(column.key) : null">
+            <slot
+              :name="`head:${column.key}`"
+              :column="column">
+              {{ column.title }}
+            </slot>
+            <a
+              v-if="column.orderable"
+              href="#"
+              :class="`slim-table-orderable ${orders[column.key] || ''}`" />
           </th>
         </tr>
       </thead>
       <tbody>
-        <component :is="rowComponent" v-for="row, i in rows" :key="`dataRow${i}`" :row="row" :columns="columns" />
+        <slot
+          v-for="(row, i) in rows"
+          name="row"
+          :row="row"
+          :index="i"
+          :columns="columns">
+          <tr :key="row.id || i">
+            <td
+              v-for="column in columns"
+              :key="column.key">
+              <slot
+                :name="`cell:${column.key}`" :row="row" :value="row[column.key]"
+                :column="column" :index="i">
+                {{ row[column.key] }}
+              </slot>
+            </td>
+          </tr>
+        </slot>
 
-        <tr v-if="syncState === 'fetched' && rows.length === 0">
-          <td :colspan="columns.length" class="text-center" :class="emptyRowsClass">
-            {{ emptyRowsText }}
-          </td>
-        </tr>
+        <slot v-if="syncState === 'fetched' && rows.length === 0" name="empty-row">
+          <tr>
+            <td :colspan="columns.length">
+              No records found
+            </td>
+          </tr>
+        </slot>
 
-        <LoadingRow v-if="syncState == 'syncing'" v-for="row, i in perPage" :key="`loadingRow${i}`" :columnsLength="columns.length" />
+        <slot v-if="syncState === 'syncing'" name="loading-row">
+          <LoadingRow
+            v-for="(row, i) in perPage"
+            :key="`loadingRow${i}`"
+            :columns-length="columns.length" />
+        </slot>
       </tbody>
     </table>
 
@@ -33,112 +68,96 @@
       prev-link-class="page-link"
       next-class="page-item"
       next-link-class="page-link"
-      prev-text="<-"
-      next-text="->"
-    />
+      prev-text="←"
+      next-text="→" />
   </div>
 </template>
 
 <script>
-  import qs from 'qs'
-  import Paginate from 'vuejs-paginate'
+import qs from 'qs'
+import Paginate from 'vuejs-paginate'
 
-  import LoadingRow from './loading_row.vue'
-  import DataRow from './data_row.vue'
+import LoadingRow from './loading_row.vue'
 
-  export default {
-    props: {
-      columns: { type: Array, required: true },
-      remoteUrl: { type: String, required: true },
-      perPage: { type: Number, default: 25 },
-      perPageKey: { type: String, default: 'per_page' },
-      customFilters: { type: Object, default() { return {} } },
-      onFetchedCallback: Function,
-      wrapperClass: String,
-      tableClass: String,
-      emptyRowsClass: String,
-      emptyRowsText: { type: String, default: 'No records found' },
-      totalRowsCountKey: { type: String, default: 'total_count' },
-      rowComponent: { type: Object, default() { return DataRow } }
-    },
-    data() {
-      return {
-        page: 1,
-        rows: [],
-        rowsTotalCount: 0,
-        syncState: 'initial',
-        orders: {}
-      }
-    },
-    computed: {
-      pagesCount() {
-        return Math.ceil(this.rowsTotalCount / this.perPage)
-      }
-    },
-    methods: {
-      reload() {
-        this.fetchData(this.page)
-      },
-      fetchData(page) {
-        const params = {
-          [this.perPageKey]: this.perPage,
-          page: page,
-          ...this.customFilters
-        }
-
-        const orderKeys = Object.keys(this.orders)
-        if (orderKeys.length) {
-          params.order = orderKeys.map(key => {
-            return { field: key, direction: this.orders[key] }
-          })
-        }
-
-        this.syncState = 'syncing'
-        this.rows = []
-
-        fetch(`${this.remoteUrl}?${qs.stringify(params, { arrayFormat: 'brackets' })}`)
-          .then(res => res.json())
-          .then(res => {
-            this.rows = res.rows
-            this.rowsTotalCount = res[this.totalRowsCountKey]
-            this.onFetchedCallback && this.onFetchedCallback(res)
-            this.syncState = 'fetched'
-          })
-      },
-      onOrderClick(key) {
-        if (this.orders[key] === 'asc') {
-          this.orders = { [key]: 'desc' }
-        } else if (this.orders[key] === 'desc') {
-          this.orders = {}
-        } else {
-          this.orders = { [key]: 'asc' }
-        }
-      },
-      refetch() {
-        if (this.page === 1) {
-          this.fetchData(1)
-        } else {
-          this.page = 1
-        }
-      }
-    },
-    watch: {
-      page(val) {
-        this.fetchData(val)
-      },
-      customFilters() {
-        this.refetch()
-      },
-      orders() {
-        this.refetch()
-      },
-      perPage() {
-        this.refetch()
-      }
-    },
-    components: { LoadingRow, DataRow, Paginate },
-    mounted() {
-      this.reload()
+export default {
+  components: { LoadingRow, Paginate },
+  props: {
+    columns: { type: Array, required: true },
+    remoteUrl: { type: String, required: true },
+    perPage: { type: Number, default: 25 },
+    perPageKey: { type: String, default: 'per_page' },
+    customFilters: { type: Object, default: () => ({}) },
+    onFetchedCallback: { type: Function, default: (rows) => rows },
+    wrapperClass: { type: String, default: '' },
+    tableClass: { type: String, default: '' },
+  },
+  data() {
+    return {
+      page: 1,
+      rows: [],
+      rowsTotalCount: 0,
+      syncState: 'initial',
+      orders: {},
     }
-  }
+  },
+  computed: {
+    pagesCount() {
+      return Math.ceil(this.rowsTotalCount / this.perPage)
+    },
+  },
+  watch: {
+    page: 'fetchData',
+    customFilters: 'refetch',
+    orders: 'refetch',
+    perPage: 'refetch',
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    reload() {
+      this.fetchData(this.page)
+    },
+    async fetchData(page = 1) {
+      const params = {
+        [this.perPageKey]: this.perPage,
+        page,
+        ...this.customFilters,
+      }
+
+      const orderKeys = Object.keys(this.orders)
+      if (orderKeys.length) {
+        params.order = orderKeys.map((key) => ({ field: key, direction: this.orders[key] }))
+      }
+
+      this.syncState = 'syncing'
+      this.rows = []
+
+      const response = await fetch(`${this.remoteUrl}?${qs.stringify(params, { arrayFormat: 'brackets' })}`)
+      const data = await response.json()
+
+      if (this.onFetchedCallback) this.onFetchedCallback(data)
+
+      this.rows = data.rows
+      this.rowsTotalCount = response.headers.get('X-Total-Count') || 0
+      this.syncState = 'fetched'
+    },
+    onOrderClick(key) {
+      if (this.orders[key] === 'asc') {
+        this.orders = { [key]: 'desc' }
+      } else if (this.orders[key] === 'desc') {
+        this.orders = {}
+      } else {
+        this.orders = { [key]: 'asc' }
+      }
+    },
+    refetch() {
+      if (this.page === 1) {
+        this.fetchData(1)
+      } else {
+        this.page = 1
+      }
+    },
+  },
+}
 </script>
