8ffaf3e3eb179b900906783817c9039993bb79d6
fLa <flatra66@gmail.com>
2021-05-09 20:32:15 +0500

    unify remote prop and slots, add readme

--- src/js/components/table.vue
+++ src/js/components/table.vue
@@ -1,113 +1,103 @@
 <template>
-  <div :class="wrapperClass">
-    <table :class="tableClass" class="slim-table">
-      <thead v-if="columns.length">
-        <tr>
-          <th
+  <table class="vst">
+    <thead v-if="columns.length">
+      <tr>
+        <th
+          v-for="column in columns"
+          :key="column.key"
+          :class="['vst-th', { 'vst-orderable': column.orderable }]"
+          @click.prevent="column.orderable ? onOrderClick(column.key) : null">
+          <slot
+            :name="`head:${column.key}`"
+            :column="column">
+            {{ column.title }}
+          </slot>
+          <a
+            v-if="column.orderable"
+            href="#"
+            :class="`vst-orderable-toggle ${orders[column.key] || ''}`" />
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <slot
+        v-for="(row, i) in rows"
+        name="row"
+        :row="row"
+        :index="i"
+        :columns="columns">
+        <tr :key="row.id || i">
+          <td
             v-for="column in columns"
-            :key="column.key"
-            :class="['slim-table-th', { 'slim-table-orderable': column.orderable }]"
-            @click.prevent="column.orderable ? onOrderClick(column.key) : null">
+            :key="column.key">
             <slot
-              :name="`head:${column.key}`"
-              :column="column">
-              {{ column.title }}
+              :name="`cell:${column.key}`"
+              :row="row"
+              :value="row[column.key]"
+              :column="column"
+              :index="i">
+              {{ row[column.key] }}
             </slot>
-            <a
-              v-if="column.orderable"
-              href="#"
-              :class="`slim-table-orderable-toggle ${orders[column.key] || ''}`" />
-          </th>
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        <slot
-          v-for="(row, i) in rows"
-          name="row"
-          :row="row"
-          :index="i"
-          :columns="columns">
-          <tr :key="row.id || i">
-            <td
-              v-for="column in columns"
-              :key="column.key">
-              <slot
-                :name="`cell:${column.key}`" :row="row" :value="row[column.key]"
-                :column="column" :index="i">
-                {{ row[column.key] }}
-              </slot>
-            </td>
-          </tr>
-        </slot>
+      </slot>
 
-        <slot v-if="syncState === 'fetched' && rows.length === 0" name="empty-row">
-          <tr>
-            <td :colspan="columns.length">
-              No records found
-            </td>
-          </tr>
-        </slot>
+      <slot v-if="syncState === 'fetched' && rows.length === 0" name="row:empty">
+        <tr>
+          <td :colspan="columns.length">
+            No records found
+          </td>
+        </tr>
+      </slot>
 
-        <slot v-if="syncState === 'syncing'" name="loading-row">
-          <LoadingRow
-            v-for="(row, i) in perPage"
-            :key="`loadingRow${i}`"
-            :columns-length="columns.length" />
-        </slot>
-      </tbody>
-    </table>
+      <slot v-if="syncState === 'syncing'" name="row:loading">
+        <LoadingRow
+          v-for="(row, i) in perPage"
+          :key="`loadingRow${i}`"
+          :columns-length="columns.length" />
+      </slot>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td :colspan="columns.length">
+          <slot name="pagination" :page="page" :rows="rows">
+            <ul class="vst-pagination mt-3">
+              <li :class="['vst-page-item', { disabled: page === 1 }]">
+                <a class="vst-page-link" @click.prevent="page -= 1">←</a>
+              </li>
 
-    <Paginate
-      v-if="pagesCount > 1"
-      v-model="page"
-      :page-count="pagesCount"
-      container-class="pagination mt-3"
-      page-class="page-item"
-      page-link-class="page-link"
-      prev-class="page-item"
-      prev-link-class="page-link"
-      next-class="page-item"
-      next-link-class="page-link"
-      prev-text="←"
-      next-text="→" />
-  </div>
+              <li :class="['vst-page-item', { disabled: rows.length < perPage }]">
+                <a class="vst-page-link" @click.prevent="page += 1">→</a>
+              </li>
+            </ul>
+          </slot>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
 </template>
 
 <script>
 import qs from 'qs'
-import Paginate from 'vuejs-paginate'
-
 import LoadingRow from './loading_row.vue'
 
 export default {
-  components: { LoadingRow, Paginate },
+  components: { LoadingRow },
   props: {
     columns: { type: Array, required: true },
-    remoteUrl: { type: String, required: true },
+    source: { type: [String, Function], required: true },
     perPage: { type: Number, default: 25 },
-    perPageKey: { type: String, default: 'per_page' },
-    customFilters: { type: Object, default: () => ({}) },
-    onFetchedCallback: { type: Function, default: (rows) => rows },
-    wrapperClass: { type: String, default: '' },
-    tableClass: { type: String, default: '' },
   },
   data() {
     return {
       page: 1,
       rows: [],
-      rowsTotalCount: 0,
       syncState: 'initial',
       orders: {},
     }
   },
-  computed: {
-    pagesCount() {
-      return Math.ceil(this.rowsTotalCount / this.perPage)
-    },
-  },
   watch: {
     page: 'fetchData',
-    customFilters: 'refetch',
     orders: 'refetch',
     perPage: 'refetch',
   },
@@ -119,11 +109,7 @@ export default {
       this.fetchData(this.page)
     },
     async fetchData(page = 1) {
-      const params = {
-        [this.perPageKey]: this.perPage,
-        page,
-        ...this.customFilters,
-      }
+      const params = { per_page: this.perPage, page }
 
       const orderKeys = Object.keys(this.orders)
       if (orderKeys.length) {
@@ -133,13 +119,15 @@ export default {
       this.syncState = 'syncing'
       this.rows = []
 
-      const response = await fetch(`${this.remoteUrl}?${qs.stringify(params, { arrayFormat: 'brackets' })}`)
-      const data = await response.json()
+      let data
+      if (typeof this.source === 'string') {
+        const response = await fetch(`${this.source}?${qs.stringify(params, { arrayFormat: 'brackets' })}`)
+        data = await response.json()
+      } else {
+        data = await this.source(params)
+      }
 
-      if (this.onFetchedCallback) this.onFetchedCallback(data)
-
-      this.rows = data.rows
-      this.rowsTotalCount = response.headers.get('X-Total-Count') || 0
+      this.rows = data
       this.syncState = 'fetched'
     },
     onOrderClick(key) {
